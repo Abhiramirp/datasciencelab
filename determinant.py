77653bca3eded9d4caa6cbdb3eeaf4bf6c2431ea
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-08-19 10:22:33 +0530

    Create determinant.py

---
+++ determinant.py
@@ -0,0 +1,22 @@
+import numpy as np
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of columns: "))
+
+
+matrix = []
+
+
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        element = int(input("Enter element for position ({i}, {j}): "))
+        row.append(element)
+    matrix.append(row)
+
+
+print("The matrix is:")
+for row in matrix:
+    print(row)
+det = np.linalg.det(matrix) 
+print("\nDeterminant of given matrix:") 
+print(int(det)) 
