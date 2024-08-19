b56204fbf066ad37cb8a0549397869cc21284f24
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-08-19 10:21:14 +0530

    Create arradd.py

---
+++ arradd.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([1,2,3,4])
+brr=np.array([5,4,3,2])
+addition_result=np.add(arr,brr)
+print("Addition:",addition_result)
+multi_result=np.multiply(arr,brr)
+print("Multiplication:",multi_result)
