33ef4209c969090f37ac6e1b1a97fb828dda915c
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-08-19 11:22:49 +0530

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,15 @@
+import numpy as np
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of columns: "))
+matrix = []
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        element = int(input(f"Enter element for position ({i}, {j}): "))
+        row.append(element)
+    matrix.append(row)
+print("The matrix is:")
+for row in matrix:
+    print(row)  
+print("Transpose is:")
+print( np.transpose(matrix))
