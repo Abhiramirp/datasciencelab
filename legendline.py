065e92c66171405897963472bde73cb63ec90f66
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-09-25 10:02:43 +0530

    Create legendline.py

---
+++ legendline.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints=np.array([1,2,6])
+ypoints=np.array([3,10,12])
+apoints=np.array([3,7,12])
+bpoints=np.array([6,10,17])
+
+plt.plot(apoints,bpoints,'g',label="student")
+plt.plot(xpoints,ypoints,'r',label="teacher")
+leg=plt.legend()
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.title("STUDENT TEACHER PERFORMANCE GRAPH")
+plt.show()
