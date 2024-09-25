3e85c7b5ea6c3b794b3f22bf7ef4af8ae2e03830
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-09-25 10:19:16 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x = [12,14,16,18,20,22,24]
+
+y = [100,200,250,400,300,400,500]
+
+plt.plot(x, y)
+
+plt.xlabel('Temperature')
+
+plt.ylabel('Sales')
+
+plt.title('Sales')
+
+plt.show()
