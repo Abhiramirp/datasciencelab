c189ad42d9d392f70283f6067e688e92bcc2b001
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-11-23 16:51:05 +0530

    Update dottedline.py

--- dottedline.py
+++ dottedline.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints=np.array([1,2,6,18])
-ypoints=np.array([3,10,12,20])
+x=np.array([1,2,6,18])
+y=np.array([3,10,12,20])
 
-plt.plot(xpoints,ypoints,'--g',marker = 'o',
+plt.plot(x,y,'--g',marker = 'o',
          markerfacecolor = 'red')
 
 plt.show()
