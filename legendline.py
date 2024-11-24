08d1e9c8f221ab9182df87de199e4095d682f93d
Abhiramirp <148995237+Abhiramirp@users.noreply.github.com>
2024-11-24 10:28:16 +0530

    Update legendline.py

--- legendline.py
+++ legendline.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints=np.array([1,2,6])
-ypoints=np.array([3,10,12])
-apoints=np.array([3,7,12])
-bpoints=np.array([6,10,17])
+x=np.array([1,2,6])
+y=np.array([3,10,12])
+a=np.array([3,7,12])
+b=np.array([6,10,17])
 
-plt.plot(apoints,bpoints,'g',label="student")
-plt.plot(xpoints,ypoints,'r',label="teacher")
+plt.plot(a,b,'g',label="student")
+plt.plot(x,y,'r',label="teacher")
 
 plt.xlabel("x axis")
 plt.ylabel("y axis")
